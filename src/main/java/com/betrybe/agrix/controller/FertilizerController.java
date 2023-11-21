d843a7631948a2984edec97bacbd2c01fe03830b
rosamenezes <g.rosamenezes1@gmail.com>
2023-11-21 09:40:50 -0300

    Req 9

--- src/main/java/com/betrybe/agrix/controller/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/controller/FertilizerController.java
@@ -6,10 +6,12 @@ import com.betrybe.agrix.service.FertilizerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * Controller.
@@ -31,5 +33,14 @@ public class FertilizerController {
   public ResponseEntity<DtoFertilizer> createFertilizer(@RequestBody DtoFertilizer fertilizerDto) {
     Fertilizer createdFertilizer = fertilizerService.createFertilizer(fertilizerDto.toFertilizer());
     return ResponseEntity.status(HttpStatus.CREATED).body(DtoFertilizer.from(createdFertilizer));
+  }
+
+  /**
+   * AllFertilizers.
+   */
+  @GetMapping
+  public ResponseEntity<List<DtoFertilizer>> getAllFertilizers() {
+    List<Fertilizer> fertilizers = fertilizerService.getAll();
+    return ResponseEntity.status(HttpStatus.OK).body(fertilizers.stream().map(DtoFertilizer::from).toList());
   }
 }
--- src/main/java/com/betrybe/agrix/service/FertilizerService.java
+++ src/main/java/com/betrybe/agrix/service/FertilizerService.java
@@ -5,6 +5,8 @@ import com.betrybe.agrix.repositories.RepoFertilizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * Fertilizer service.
  */
@@ -13,8 +15,17 @@ public class FertilizerService {
   @Autowired
   private RepoFertilizer repoFertilizer;
 
-
+  /**
+   * Create Fertilizer.
+   */
   public Fertilizer createFertilizer(Fertilizer fertilizer) {
     return repoFertilizer.save(fertilizer);
+  }
+
+  /**
+   * Get All Fertilizers.
+   */
+  public List<Fertilizer> getAll() {
+    return repoFertilizer.findAll();
   }
 }
