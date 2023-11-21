8d6c359dc5e81795d7151baf1ceea628de51bebc
rosamenezes <g.rosamenezes1@gmail.com>
2023-11-21 01:27:37 -0300

    error

--- src/main/java/com/betrybe/agrix/repositories/RepoCrop.java
+++ src/main/java/com/betrybe/agrix/repositories/RepoCrop.java
@@ -3,7 +3,6 @@ package com.betrybe.agrix.repositories;
 import com.betrybe.agrix.entities.Crop;
 import java.time.LocalDate;
 import java.util.List;
-import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
